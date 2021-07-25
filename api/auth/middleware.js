f61bccf1c786a98df00637cc1c7530c887b3ac48
Ethan Edmond <eedmond@uccs.edu>
2021-07-25 17:35:40 -0600

    Got [POST] /api/auth/register passing missing username and password test

--- api/auth/middleware.js
+++ api/auth/middleware.js
@@ -1,4 +1,10 @@
-exports.validateBody = (req, res, next) => {
-  console.log('validateBody wired');
-  next();
+exports.validateBody = ({body: {username, password}}, res, next) => {
+  if (username && password) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: 'Please provide a username and password'
+    });
+  }
 };
