e89935d22423ed2389b9f11df89fa7b93cf481c3
Ethan Edmond <eedmond@uccs.edu>
2021-07-25 17:33:58 -0600

    Wired validateBody middleware to register endpoint

---
+++ api/auth/middleware.js
@@ -0,0 +1,4 @@
+exports.validateBody = (req, res, next) => {
+  console.log('validateBody wired');
+  next();
+};
--- api/auth/router.js
+++ api/auth/router.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
+const {
+  validateBody
+} = require('./middleware');
 
-router.post('/register', (req, res, next) => {
+router.post('/register', validateBody, (req, res, next) => {
   next({
     status: 404,
     message: 'Not implemented'
