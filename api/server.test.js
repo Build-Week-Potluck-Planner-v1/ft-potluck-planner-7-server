41e164cf3fc688d571506f703f5bc617a011a39d
Ethan Edmond <eedmond@uccs.edu>
2021-07-25 17:22:12 -0600

    Added a bunch of test todos for register endpoint

--- api/server.test.js
+++ api/server.test.js
@@ -21,4 +21,17 @@ describe('server.js', () => {
   it('is the correct testing environment', async () => {
     expect(process.env.NODE_ENV).toBe('testing');
   });
+
+  describe('users', () => {
+
+    describe('[POST] /api/auth/register', () => {
+      it.todo('Responds with a 400 and a message on missing password or username');
+      it.todo('Responds with a 400 and a message on bad typing');
+      it.todo('Responds with a 400 and a message on existing username');
+      it.todo('Adds a new user to the system');
+      it.todo('Responds with 201 on good register');
+      it.todo('Responds with user id and username on good register');
+    });
+
+  });
 });
