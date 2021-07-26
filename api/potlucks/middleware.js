30e2e23289c9e58c4fa0893424a8b299d64d6b28
Ethan Edmond <eedmond@uccs.edu>
2021-07-25 22:15:09 -0600

    Got validatePotluck passing missing info test

--- api/potlucks/middleware.js
+++ api/potlucks/middleware.js
@@ -1,4 +1,10 @@
-exports.validatePotluck = (req, res, next) => {
-  console.log('validatePotluck wired');
-  next();
+exports.validatePotluck = ({body: {name, date, time, location}}, res, next) => {
+  if (name && date && time && location) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: 'Please provide a name, date, time and location for the potluck'
+    });
+  }
 };
