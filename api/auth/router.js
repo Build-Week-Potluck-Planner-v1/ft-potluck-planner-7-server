d43165e83146a0a8fcdacba066a419b61852e135
Ethan Edmond <eedmond@uccs.edu>
2021-07-25 18:07:22 -0600

    Wired checkUsernameFree middleware

--- api/auth/router.js
+++ api/auth/router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const {
   validateBody,
-  validateType
+  validateType,
+  checkUsernameFree
 } = require('./middleware');
 
-router.post('/register', validateBody, validateType, (req, res, next) => {
+router.post('/register', validateBody, validateType, checkUsernameFree, (req, res, next) => {
   next({
     status: 404,
     message: 'Not implemented'
