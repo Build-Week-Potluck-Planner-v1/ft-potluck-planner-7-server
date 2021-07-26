9a9c7c28b07e7ef96edc228707d36cb8d5e847f3
Ethan Edmond <eedmond@uccs.edu>
2021-07-25 18:11:49 -0600

    Wrote test for getByUsername when user exists

--- api/auth/model.test.js
+++ api/auth/model.test.js
@@ -1,5 +1,16 @@
+const Users = require('./model');
+const db = require('../data/db-config');
 
 describe('getByUsername', () => {
-  it.todo('returns user when user exists');
+
+  it('returns user when user exists', async () => {
+    const user = await Users.getByUsername('test1');
+    expect(user).toMatchObject({
+      id: 1,
+      username: 'test1',
+      password: '1234',
+    });
+  });
+
   it.todo('returns nothing when user doesnt');
 });
