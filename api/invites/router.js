ac0e2450bf804e0ec6fff150cbac3f056566e106
Ethan Edmond <eedmond@uccs.edu>
2021-07-28 14:41:57 -0600

    Finished [DELETE] /api/invites/:id endpoint

--- api/invites/router.js
+++ api/invites/router.js
@@ -12,7 +12,9 @@ const {
   validateTypePut,
   checkInviteExistsPut,
   checkUserIsGuest,
-  updateInvite
+  updateInvite,
+  checkUserIsOwnerOrGuest,
+  deleteInvite
 } = require('./middleware');
 
 router.get('/', getInvites, ({invites}, res, next) => {
@@ -33,15 +35,16 @@ const putMiddleware = [
   updateInvite
 ];
 
-router.put('/:id', putMiddleware, (req, res, next) => {
-  res.json(req.updated);
+router.put('/:id', putMiddleware, ({updated}, res, next) => {
+  res.json(updated);
 });
 
-router.delete('/:id', (req, res, next) => {
-  next({
-    status: 404,
-    message: 'Not implemented'
-  });
+const deleteMiddleware = [
+  checkInviteExistsPut, checkUserIsOwnerOrGuest, deleteInvite
+];
+
+router.delete('/:id', deleteMiddleware, ({deleted}, res, next) => {
+  res.json(deleted);
 });
 
 module.exports = router;
